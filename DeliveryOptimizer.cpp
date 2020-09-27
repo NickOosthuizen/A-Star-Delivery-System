55553579ec87fcc2b5cdb2d12bb6f9e8b7223dab
Nick Oosthuizen <nickoosthuizen@g.ucla.edu>
2020-09-27 10:59:49 -0500

    Changed optimizer implementation to simulated annealing

--- DeliveryOptimizer.cpp
+++ DeliveryOptimizer.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 using namespace std;
 
+
+double getDistance(const vector<DeliveryRequest>& deliveries, const GeoCoord& depot);
+
 class DeliveryOptimizerImpl
 {
 public:
@@ -12,6 +15,8 @@ public:
         vector<DeliveryRequest>& deliveries,
         double& oldCrowDistance,
         double& newCrowDistance) const;
+	double acceptChance(double distance, double newDistance, double temp) const;
+
 };
 
 DeliveryOptimizerImpl::DeliveryOptimizerImpl(const StreetMap* sm)
@@ -22,52 +27,82 @@ DeliveryOptimizerImpl::~DeliveryOptimizerImpl()
 {
 }
 
+
+double DeliveryOptimizerImpl::acceptChance(double distance, double newDistance, double temp) const
+{
+	double chance;
+	//chance will be accepted
+	if (newDistance < distance)
+		chance = 1;
+	//when temp is higher, a larger number will be returned
+	//this means the optimizer will have a higher chance of accepting a worse solution earlier on
+	else
+		chance = exp((distance - newDistance) / temp);
+	return chance;
+}
+
+//Gets the distance of entire route
+double getDistance(const vector<DeliveryRequest>& deliveries, const GeoCoord& depot)
+{
+	int distance = 0;
+	GeoCoord start = depot;
+	if (deliveries.size() < 1)
+		return distance;
+	
+	for (int i = 0; i < deliveries.size(); i++)
+	{
+		distance += distanceEarthMiles(start, deliveries[i].location);
+		start = deliveries[i].location;
+	}
+	distance += distanceEarthMiles(start, depot);
+
+	return distance;
+}
+
+//simulated annealing algorithm to optimize delivery order
 void DeliveryOptimizerImpl::optimizeDeliveryOrder(
     const GeoCoord& depot,
     vector<DeliveryRequest>& deliveries,
     double& oldCrowDistance,
     double& newCrowDistance) const
 {
-	oldCrowDistance = 0;
-	GeoCoord start = depot;
-	for (int i = 0; i < deliveries.size(); i++)
-	{
-		oldCrowDistance += distanceEarthMiles(start, deliveries[i].location);
-		start = deliveries[i].location;
-	}
-	oldCrowDistance += distanceEarthMiles(start, depot);
-	
-	vector<DeliveryRequest> optimizedDeliveries;
-	start = depot;
-	
-	for (int i = 0; i < deliveries.size(); )
-	{
-		DeliveryRequest optimalSpot(deliveries[i].item, deliveries[i].location);	//attempt ten random position in deliveries to try to find an optimal ordering
-		int optimalSpotIndex = i;
-		for (int j = 0; j < 10; j++)
-		{
-			int position = rand() % deliveries.size();
-			double distance = distanceEarthMiles(start, optimalSpot.location);
-			if (distanceEarthMiles(deliveries[position].location, start) < distance)
-			{
-				optimalSpot = deliveries[position];
-				optimalSpotIndex = i;
-			}
-		}
-		start = optimalSpot.location;
-		optimizedDeliveries.push_back(optimalSpot);
-		deliveries.erase(deliveries.begin() + optimalSpotIndex);
-	}
-	deliveries = optimizedDeliveries;
+	double temp = 10000;
+	double coolingRate = 0.003;
 
-	start = depot;
-	newCrowDistance = 0;
-	for (int i = 0; i < deliveries.size(); i++)
+	oldCrowDistance = getDistance(deliveries, depot);
+	vector<DeliveryRequest> currentRoute = deliveries;
+	vector<DeliveryRequest> optimizedDeliveries = currentRoute;
+
+	while (temp > 1)
 	{
-		newCrowDistance += distanceEarthMiles(start, deliveries[i].location);
-		start = deliveries[i].location;
+		vector<DeliveryRequest> newRoute = deliveries;
+
+		//randomly switch 2 positions in the route
+		int position1 = rand() % deliveries.size();
+		int position2 = rand() % deliveries.size();
+
+		DeliveryRequest tempDeliv = newRoute[position1];
+		newRoute[position1] = newRoute[position2];
+		newRoute[position2] = tempDeliv;
+	
+		int currentDistance = getDistance(currentRoute, depot);
+		int newDistance = getDistance(newRoute, depot);
+
+		//if the accept chance (between 0-1) is greater than than a 
+		//rand chance (0-1) accept it as the current solution
+		//exploring random choices decreases the chance of getting stuck in a local optimum
+		if (acceptChance(currentDistance, newDistance, temp) > (rand() / RAND_MAX))
+			currentRoute = newRoute;
+
+		//if the current route is shorter than the best current route, take it as the route
+		if (getDistance(currentRoute, depot) < getDistance(optimizedDeliveries, depot))
+			optimizedDeliveries = currentRoute;
+
+		temp *= 1 - coolingRate;
 	}
-	newCrowDistance += distanceEarthMiles(start, depot);
+
+	deliveries = optimizedDeliveries;
+	newCrowDistance = getDistance(deliveries, depot);
 
 }
 
